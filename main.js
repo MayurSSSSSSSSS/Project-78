e96396f318527134f499e51330f8ddb38f6a7f1c
MayurSSSSSSSSS <91515050+MayurSSSSSSSSS@users.noreply.github.com>
2021-10-11 19:12:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var images = ["https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
+var names = ["Grandpa", "Father", "Brother", "Sister","Mother"];
+var i = 0;
+function update()
+{
+   
+    var numbers_of_family_member_in_array = 4;
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+    //Debug the code to store list of images in updatedImage. Use images[i]
+    var updatedImage = images[i];
+    //Debug the code to store list of names in updatedName. Use names[i]
+    var updatedName = names[i] ;
+ 
+    document.getElementById("Image").src = updatedImage;
+    document.getElementById("Heding").innerHTML = updatedName;
+
+     i++;
+}
